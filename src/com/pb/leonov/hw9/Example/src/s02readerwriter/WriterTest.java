ddea0b87b54dc5da861207e3c6a53f4683ea0b6d
Tony-Leonov <anton.leonov.dp@gmail.com>
2021-11-22 23:47:03 +0200

    hw - 9

--- src/com/pb/leonov/hw9/Example/src/s02readerwriter/WriterTest.java
+++ src/com/pb/leonov/hw9/Example/src/s02readerwriter/WriterTest.java
@@ -5,7 +5,7 @@ import java.io.Writer;
 
 public class WriterTest {
     public static void main(String[] args) {
-        String data = "This is the data in the output file";
+        String data = "This is the data in the output file123123";
 
         // Creates a Writer using FileWriter
         try (Writer writer = new FileWriter("files/s02-writer.txt")) {
