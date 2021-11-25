d7bd7716c1f9843dca7a1af6a650bd2fcc77eed1
Tony-Leonov <anton.leonov.dp@gmail.com>
2021-11-25 23:35:03 +0200

    hw - 9

--- src/com/pb/leonov/hw9/FileNumbers.java
+++ src/com/pb/leonov/hw9/FileNumbers.java
@@ -1,25 +1,19 @@
 package com.pb.leonov.hw9;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class FileNumbers {
-
-
     public static void main(String[] args) throws IOException {
-
         createNumbersFile();
         createOddNumbersFile();
-
     }
 
     public static void createNumbersFile() {
 
         // Creates a Writer using FileWriter
-        try (Writer writer = new FileWriter("src\\com\\pb\\leonov\\hw9\\test_write1.txt")) {
+        try (Writer writer = new FileWriter("src\\com\\pb\\leonov\\hw9\\numbers.txt")) {
 
             int[][] array = new int[10][10];
 
@@ -40,35 +34,38 @@ public class FileNumbers {
     }
 
     public static void createOddNumbersFile() throws IOException {
-        int size = (int) Files.size(Paths.get("src\\com\\pb\\leonov\\hw9\\test_write1.txt"));
+        System.out.println("-----------------------------------\nОбнуление четных чисел и перезапись в odd-numbers.txt ");
 
-        // Creates an array of character
-        int[] arrayq = new int[101];
+        String path = "src\\com\\pb\\leonov\\hw9\\numbers.txt";
 
-        // Creates a reader using the FileReader
-        try (Reader reader = new FileReader("src\\com\\pb\\leonov\\hw9\\test_write1.txt")) {
+        File file = new File(path);
 
-            // Checks if reader is ready
-            System.out.println("\nIs there data in the stream?  " + reader.ready());
+        Scanner scanner = new Scanner(file);
 
+        try(Writer writer = new FileWriter("src\\com\\pb\\leonov\\hw9\\odd-numbers.txt")) {
 
-            System.out.println("Data in the stream:");
-            System.out.println(arrayq);
+            int[][] array = new int[10][10];
 
-            for (int i = 0; i < arrayq.length; i++) {
-                if(i%2 == 1){
-                    arrayq[i] = 0;
+            for (int i = 0; i < array.length; i++, System.out.println()) {
+                for (int j = 0; j < array.length; j++) {
+                    while (scanner.hasNextInt()) {
+                        int qwe = Integer.parseInt(String.valueOf(scanner.nextInt()));
+                        if (qwe % 2 == 0) {
+                            qwe = 0;
+                        }
+                        array[i][j] = qwe;
+
+                        System.out.print(array[i][j] + " ");
+                        writer.write(array[i][j] + " ");
+                        break;
+                    }
                 }
+                writer.write("\n");
             }
-            System.out.println(Arrays.toString(arrayq));
-            //System.out.println(arrayq);
 
-
-
-
-        } catch(Exception e) {
-            e.printStackTrace();
+        }  catch (Exception ex) {
+            System.out.println("Error with file read: " + ex);
         }
-
+        System.out.println("Read from file done!");
     }
 }
