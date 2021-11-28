7721adb25366c65aa29508ddfa16e4a7b790e75c
Tony-Leonov <anton.leonov.dp@gmail.com>
2021-11-28 22:24:36 +0200

    hw - 10

---
+++ src/com/pb/leonov/hw10/Main.java
@@ -0,0 +1,26 @@
+package com.pb.leonov.hw10;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //протестировать полученный класс на примере NumBox<Float> и NumBox<Integer>.
+        //C использованием дженериков
+        NumBox<Float> floatNumBox  = new NumBox<>(5);
+
+        floatNumBox.add(0,32);         System.out.println();
+
+
+
+
+
+//        Float1.add("2.52");
+//        Float2.add("1.12");
+        // Ошибка компиляции
+        // list2.add(3);
+
+//        Number s1 = Float.get(0);  System.out.println(s1);
+//        Number s2 = Float.get(1);  System.out.println(s2);
+//        Number s3 = Float.get(2);  System.out.println(s3);
+
+    }
+}
--- src/com/pb/leonov/hw10/NumBox.java
+++ src/com/pb/leonov/hw10/NumBox.java
@@ -1,4 +1,27 @@
 package com.pb.leonov.hw10;
 
-public class NumBox {
+public class NumBox <T>  {
+
+    private T[] arr;
+    int size;
+
+    public NumBox(int size) {
+        size = this.size;
+    }
+
+
+    public T[] getArr() {
+        return arr;
+    }
+
+    public void setArr(T[] arr) {
+        this.arr = arr;
+    }
+
+
+    void add(T num) {
+        arr.add(t);
+    }//добавляющий число в массив. В случае если массив полон - выбросить исключение.
+
+
 }
